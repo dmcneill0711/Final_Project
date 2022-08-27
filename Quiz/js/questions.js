afac0bf17dff0500bd2b7caec55d671be9f96ae6
dmcneill0711_273783 <dmcneill0711@hotmail.com>
2022-08-27 11:10:33 -0400

    Added Quiz Code

---
+++ Quiz/js/questions.js
@@ -0,0 +1,30 @@
+// Questions As An Array Of Objects
+var questions = [
+    {
+    title: 'How Old Are You?',
+    choices: ['Young, and Sprightly! Not A Day Over 40!',
+            'Somewhere Between 40 and 60-ish?...', 
+            'HOW RUDE! Do They Not Teach You Kids Manners Anymore?!'],
+    highRisk: 'HOW RUDE! Do They Not Teach You Kids Manners Anymore?!',
+    mediumRisk: 'Somewhere Between 40 and 60-ish?...',
+    lowRisk: 'Young, and Sprightly! Not A Day Over 40!'
+    },
+    {
+    title: 'If You Had To Guess, How Many Calories/Grams Of Added Sugar Do You Consume In A Day? (*Do Not Think Too Hard, This Is Only An Estimate*)',
+    choices: ['Added Sugar? Gross. I Am Definitely More Of A Salty Person Than Sweet.', 
+            'Umm,... Around 200 Calories/50 Grams, I Presume', 
+            'This Is America! There Is Added Sugar In EVERYTHING! I Can Not Possibly Keep Count, But I Am Sure It Is More Than The 2nd Option.'],
+    highRisk: 'This Is America! There Is Added Sugar In EVERYTHING! I Can Not Possibly Keep Count, But I Am Sure It Is More Than The 2nd Option.',
+    mediumRisk: 'Umm,... Around 200 Calories/50 Grams, I Presume',
+    lowRisk: 'Added Sugar? Gross. I Am Definitely More Of A Salty Person Than Sweet.'
+    },
+    {
+    title: 'Although We Here At fUZZbEED Understand The Highly Problematic Nature Of The BMI, We Just HAVE To Ask... What Is Yours?',
+    choices: ['Ew! How Problematic Of You To Be Asking Me This, But If You Must Know... It Is Below 18.5.', 
+        'I Am Equally Disgusted! Mine Is Between 18.5 & 25. Now Leave Me Alone!', 
+        'My Sentiments Match The Two Fine Answers Prior! It Is Over 25 Thank You! Begone!'],
+    highRisk: 'My Sentiments Match The Two Fine Answers Prior! It Is Over 25 Thank You! Begone!',
+    mediumRisk: 'Ew! How Problematic Of You To Be Asking Me This, But If You Must Know... It Is Below 18.5.',
+    lowRisk: 'I Am Equally Disgusted! Mine Is Between 18.5 & 25. Now Leave Me Alone!'
+    },
+  ];
